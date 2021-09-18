0b680969c968d75d29eeb60ab292e29f75bb76c7
Ali Ollaik <ollaikali@gmail.com>
2021-09-18 07:58:15 -0600

    Added "Save" button and "previous entries" counter

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 
 let count = 0
 
 function increment() {
     count = count + 1
     countEl.innerText = count
+}
+
+function save() {
+    let countText = count + " - "
+
+    saveEl.innerText += countText
 }
