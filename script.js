d6a3dbedba30d23689428bf02e966abb546ffe18
Ali Ollaik <ollaikali@gmail.com>
2021-09-19 08:21:15 -0600

    App is almost complete. Added Total button but no JS for it yet

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 let countEl = document.getElementById("count-el")
 let saveEl = document.getElementById("save-el")
+let totalEl = document.getElementById("total-el")
 
 let count = 0
 
 function increment() {
-    count = count + 1
-    countEl.innerText = count
+    count += 1
+    countEl.textContent = count
 }
 
 function save() {
     let countText = count + " - "
+    saveEl.textContent += countText
+    countEl.textContent = 0
+    count = 0
+    }
 
-    saveEl.innerText += countText
+function total() {
+    
 }
