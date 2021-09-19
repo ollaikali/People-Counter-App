89b81dd1b080a0a949a1c6eb6ef8c710b9f1611c
Ali Ollaik <ollaikali@gmail.com>
2021-09-19 11:43:47 -0600

    App is done. Next!

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let saveEl = document.getElementById("save-el")
 let totalEl = document.getElementById("total-el")
 
 let count = 0
+let totalNum = 0
 
 function increment() {
     count += 1
@@ -11,11 +12,17 @@ function increment() {
 
 function save() {
     let countText = count + " - "
+    totalNum += count
     saveEl.textContent += countText
     countEl.textContent = 0
     count = 0
     }
 
 function total() {
-    
+    if (totalNum > 0) {
+        totalEl.textContent = totalNum
+    }
+    else {
+        alert ("Your total is 0")
+    }
 }
